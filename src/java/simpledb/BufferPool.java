8c517f9e2edb40ad8814526de3e1251e7230e449
Jeffrey Deng <jdeng92@gmail.com>
2013-11-23 01:26:17 -0800

    derp

--- src/java/simpledb/BufferPool.java
+++ src/java/simpledb/BufferPool.java
@@ -301,27 +301,64 @@ public class BufferPool {
      */
     public void transactionComplete(TransactionId tid, boolean commit)
         throws IOException {
-        Collection<Page> pages = myPages.values();
-        if (commit) {
-            for (Page p : pages) {
-                if (p.isDirty() == null) {
+        // Collection<Page> pages = myPages.values();
+        // if (commit) {
+        //     for (Page p : pages) {
+        //         if (p.isDirty() == null) {
+        //             p.setBeforeImage();
+        //         } else {
+        //             if (p.isDirty().equals(tid)) {
+        //                 flushPage(p.getId());
+        //             }
+        //         }
+        //     }
+        // } else {
+        //     for (Page p : pages) {  
+        //         if (p.isDirty() != null && p.isDirty().equals(tid)) {
+        //             PageId pid = p.getId();
+        //             Page beforeImage = p.getBeforeImage();
+        //             myPages.put(pid, beforeImage);
+        //         }
+        //     }
+        // }
+        // myLock.releaseAllLocks(tid);
+
+            if(commit){
+           // flush dirty pages associated to the transaction to disk
+           // flushPages(tid);
+         //flush dirty pages associated to the transaction to disk
+            Collection<Page> allPages = BPoolPageMap.values();
+            for(Page p : allPages){
+                if(p.isDirty() != null && p.isDirty().equals(tid)){
+                    
+                    flushPage(p.getId());
+                    // use current page contents as the before-image
+                    // for the next transaction that modifies this page.
                     p.setBeforeImage();
-                } else {
-                    if (p.isDirty().equals(tid)) {
-                        flushPage(p.getId());
-                    }
+                    
+                }
+                if(p.isDirty() == null){
+                    p.setBeforeImage();
+                    
                 }
             }
-        } else {
-            for (Page p : pages) {  
-                if (p.isDirty() != null && p.isDirty().equals(tid)) {
-                    PageId pid = p.getId();
-                    Page beforeImage = p.getBeforeImage();
-                    myPages.put(pid, beforeImage);
-                }
-            }
-        }
-        myLock.releaseAllLocks(tid);
+                    
+           lockManager.releaseAllLocks(tid);
+       } else {
+           //aborted
+           //revert any changes made by the transaction by restoring the page to its on-disk state. 
+           Collection<Page> p = BPoolPageMap.values();
+           for(Page page : p){
+               if(page.isDirty() != null && page.isDirty().equals(tid)){
+                   PageId pid = page.getId();
+                   //page.setBeforeImage();
+                   Page oldPage = page.getBeforeImage();
+                   BPoolPageMap.put(pid, oldPage);
+                   
+               }
+           }
+           lockManager.releaseAllLocks(tid);
+       }
     }
     /**
      * Add a tuple to the specified table behalf of transaction tid.  Will
